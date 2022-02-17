d0bd018763a958eceaaf7550bddf624cee51b837
JoseLuisLopezManuel <99145978+JoseLuisLopezManuel@users.noreply.github.com>
2022-02-16 19:46:21 -0600

    add files from cloud gui

---
+++ openKata10.py
@@ -0,0 +1,23 @@
+"""
+def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main()
+"""
+
+"""
+try:
+     open('config.txt')
+except FileNotFoundError:
+     print("Couldn't find the config.txt file!")
+     """
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
